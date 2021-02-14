caa3edec375c3c5374effad55290fa79f6bfcb5b
papp_attila <E1l2e3t4>
2021-02-14 23:13:21 +0100

    Initial commit

---
+++ src/main/java/com/example/tictactoespringangular/TicTacToeSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoespringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tictactoespringangular/TicTacToeSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictactoespringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
